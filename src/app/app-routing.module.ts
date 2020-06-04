6469388a1949d71d23e38ef9c48a69d4dd3f982a
Kumar, Mukesh <mukesh_kumar87@optum.com>
2020-06-04 17:48:37 +0530

    hi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WorkerFormComponent } from './worker-form/worker-form.component';
+import { WorkerDetailComponent } from './worker-detail/worker-detail.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'shramikForm', component: WorkerFormComponent },
+  { path: 'shramikDetail', component: WorkerDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WorkerFormComponent } from './worker-form/worker-form.component';
+import { WorkerDetailComponent } from './worker-detail/worker-detail.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import {MatGridListModule} from '@angular/material/grid-list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule,MatIconModule} from '@angular/material';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkerFormComponent,
+    WorkerDetailComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatGridListModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
